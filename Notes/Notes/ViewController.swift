609f0cd20091df22cff8eed78b18ebe9d91e0e8b
Temirlan <aboutev.kg@gmail.com>
2023-03-23 14:53:58 +0600

    Создали проект

---
+++ Notes/Notes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Toktosunov on 23/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
